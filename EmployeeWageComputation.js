fb4ce3003a422cb5aea89387347435d425a330c1
santhoshkn011 <santhoshkn011@gmail.com>
2022-08-02 09:44:34 +0530

    Added: Refactor Using Function.

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -1,27 +1,30 @@
-/*    UC2
-Ability to Check Employee is present or Absent And Calculate Wage
+/*       UC3
+Refactor the Code to write a function to get work hours
 */
 console.log("Welcome to the employee wage programs.");
 const PART_TIME_HRs=4;
 const FULL_TIME_HRs=8;
 const WAGE_PER_HRs=20;
 
-let emphrs = 0;
-let employeeCheck = Math.floor(Math.random()*10)%3;
-console.log(employeeCheck);
+//Using Function
+function getWorkingHrs(employeeCheck){
 switch(employeeCheck){
     case 0:
-        console.log("Employee worked for half day.");
-        emphrs = PART_TIME_HRs;
+        console.log("Employee is Present for Part Time.");
+        return PART_TIME_HRs;
         break;
     case 1:
-        console.log("Employee worked for full time.");
-        emphrs = FULL_TIME_HRs;
+        console.log("Employee is Present for Full Time.");
+        return FULL_TIME_HRs;
         break;
     default:
-        emphrs = 0;
-        console.log("Employee is Absent.");
+        console.log("Employee is ABSENT ");
+    return 0;    
         break;        
 }
+}
+
+let employeeCheck = Math.floor(Math.random()*10)%3;
+let emphrs = getWorkingHrs(employeeCheck)
 let empWage = emphrs * WAGE_PER_HRs;
-console.log("Employe wage for the day: $" +empWage);
+console.log("Employee worked for Hours: "+emphrs+ " \nEmploye wage earned for a day: $" +empWage);
