13f497d54cf7e2bba908bdb6d88c68117c8ae18c
santhoshkn011 <santhoshkn011@gmail.com>
2022-08-02 09:32:35 +0530

    Added: Calculated daily wage.

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -1,17 +1,27 @@
-/*--------Employee Wage-------*/
-console.log("Welcome to the employee wage programs.");
-/*
-Ability to check Employee is present or absent by using Random Function
+/*    UC2
+Ability to Check Employee is present or Absent And Calculate Wage
 */
-const IS_ABSENT = 0;
-let employeeCheck = Math.floor(Math.random() * 10 ) % 2; 
+console.log("Welcome to the employee wage programs.");
+const PART_TIME_HRs=4;
+const FULL_TIME_HRs=8;
+const WAGE_PER_HRs=20;
+
+let emphrs = 0;
+let employeeCheck = Math.floor(Math.random()*10)%3;
 console.log(employeeCheck);
-
-//Checking employee is present or absent
-//Present=1    Absent=0
-
-if(employeeCheck == IS_ABSENT){
-    console.log("Employee is ABSENT.");
-}else{
-    console.log("Employee is PRESENT.");
+switch(employeeCheck){
+    case 0:
+        console.log("Employee worked for half day.");
+        emphrs = PART_TIME_HRs;
+        break;
+    case 1:
+        console.log("Employee worked for full time.");
+        emphrs = FULL_TIME_HRs;
+        break;
+    default:
+        emphrs = 0;
+        console.log("Employee is Absent.");
+        break;        
 }
+let empWage = emphrs * WAGE_PER_HRs;
+console.log("Employe wage for the day: $" +empWage);
