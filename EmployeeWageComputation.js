14c4ad2061b880b2d133c616d2001157f54091c6
santhoshkn011 <santhoshkn011@gmail.com>
2022-08-02 10:06:02 +0530

    Added: Calculated the wage for max hours or days is reached.

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -1,11 +1,13 @@
-/*       UC3
-Refactor the Code to write a function to get work hours
+/*       UC5
+Calculate Wages till a condition of total working hours of 160 or
+max days of 20 is reached for a month.
 */
 console.log("Welcome to the employee wage programs.\n");
 const PART_TIME_HRs=4;
 const FULL_TIME_HRs=8;
 const WAGE_PER_HRs=20;
 const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
 
 //Using Function
 function getWorkingHrs(employeeCheck){
@@ -26,9 +28,11 @@ switch(employeeCheck){
 }
 
 let totalEmpHrs = 0;
-for(let day = 0; day < NUM_OF_WORKING_DAYS; day++){
+let totalWorkingDays = 0;
+while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
     let employeeCheck = Math.floor(Math.random()*10)%3;
     totalEmpHrs += getWorkingHrs(employeeCheck);
 }
 let empWage = totalEmpHrs * WAGE_PER_HRs;
-console.log("Total Hours Worked: "+totalEmpHrs+ " \nEmploye wage for the month: $" +empWage);
+console.log("Total Days Worked: " + totalWorkingDays + "\nTotal Hours Worked: "+totalEmpHrs+ " \nEmploye wage for the month: $" +empWage);
